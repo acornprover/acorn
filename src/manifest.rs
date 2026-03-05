995cf1d5d5079b45224670d72585767697e823d6
Kevin Lacker <lacker@gmail.com>
2026-03-04 16:21:16 -0800

    naw flip -> 8

--- src/manifest.rs
+++ src/manifest.rs
@@ -57,6 +57,9 @@ impl From<serde_json::Error> for ManifestError {
 /// The current version of the build format.
 /// Increment this when making breaking changes to the manifest structure, or to the structure
 /// of other components of the cached build.
+#[cfg(feature = "naw")]
+const MANIFEST_VERSION: u32 = 8;
+#[cfg(not(feature = "naw"))]
 const MANIFEST_VERSION: u32 = 7;
 
 /// A newtype wrapper for module names, created by joining parts with "."
